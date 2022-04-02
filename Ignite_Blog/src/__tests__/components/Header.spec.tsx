c338787493dfabd75aa64a85c5d8e8c11850b2e1
Yuri-Stack <yurioliveirasilva1999@gmail.com>
2022-04-02 15:16:04 -0300

    Config.: Configuring Prismic CMS in the Application

--- Ignite_Blog/src/__tests__/components/Header.spec.tsx
+++ Ignite_Blog/src/__tests__/components/Header.spec.tsx
@@ -1,3 +1,7 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+
 import { render, screen, fireEvent } from '@testing-library/react';
 import { RouterContext } from 'next/dist/next-server/lib/router-context';
 
--- Ignite_Blog/src/pages/index.tsx
+++ Ignite_Blog/src/pages/index.tsx
@@ -1,3 +1,7 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+
 import { GetStaticProps } from 'next';
 
 import { getPrismicClient } from '../services/prismic';
