06e1cac4bd75470f1ce49e0ef9fb4fae6371a676
Yuri-stack <yurioliveirasilva1999@gmail.com>
2022-04-04 22:36:44 -0300

    Styles: Stylizing the Index

--- Ignite_Blog/src/pages/_app.tsx
+++ Ignite_Blog/src/pages/_app.tsx
@@ -3,10 +3,16 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 
 import { AppProps } from 'next/app';
+import Header from '../components/Header';
 import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp;
