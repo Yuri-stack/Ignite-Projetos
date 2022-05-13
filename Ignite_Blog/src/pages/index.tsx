282f5313d572759e40e36da2cdbb79ac199e903c
Yuri-stack <yurioliveirasilva1999@gmail.com>
2022-05-13 13:09:15 -0300

    Fix: Consertando códigos para a validação de testes

--- Ignite_Blog/src/pages/index.tsx
+++ Ignite_Blog/src/pages/index.tsx
@@ -8,6 +8,8 @@ import Head from 'next/head';
 import Link from 'next/link';
 import Prismic from '@prismicio/client';
 
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR'
 import { FiCalendar, FiUser } from 'react-icons/fi';
 import { getPrismicClient } from '../services/prismic';
 
@@ -42,30 +44,28 @@ export default function Home({ postsPagination }: HomeProps) {
     setNextPage(postsPagination.next_page)
   }, [postsPagination])
 
-  const loadPosts = async(): Promise<void> => {
-    if(nextPage){
+  const loadPosts = async (): Promise<void> => {
+    if (nextPage) {
       fetch(nextPage)
-      .then(resp => resp.json())
-      .then(data => {
+        .then(resp => resp.json())
+        .then(data => {
 
-        const newPosts = data.results.map((post: Post) => ({
-          uid: post.uid,
-          first_publication_date: new Date(post.first_publication_date).toLocaleDateString('pt-BR', {
-            day: '2-digit', month: 'long', year: 'numeric'
-          }),
-          data: {
-            title: post.data.title,
-            subtitle: post.data.subtitle,
-            author: post.data.author,
-          },
-        }))
+          const newPosts = data.results.map((post: Post) => ({
+            uid: post.uid,
+            first_publication_date: post.first_publication_date,
+            data: {
+              title: post.data.title,
+              subtitle: post.data.subtitle,
+              author: post.data.author,
+            },
+          }))
 
-        setNextPage(data.nextPage)
-        setPosts([...posts, ...newPosts])
-      })
-      .catch(() => {
-        console.error('Error fecthing new page.');
-      });
+          setNextPage(data.nextPage)
+          setPosts([...posts, ...newPosts])
+        })
+        .catch(() => {
+          console.error('Error fecthing new page.');
+        });
     }
   }
 
@@ -79,26 +79,26 @@ export default function Home({ postsPagination }: HomeProps) {
         <div className={styles.posts}>
 
           {
-            posts &&
-              posts.map(post => (
-                <Link href={`/post/${ post.uid }`}>
-                  <article key={ post.uid }>
-                    <strong>{ post.data.title }</strong>
-                    <p>{ post.data.subtitle }</p>
 
-                    <div className={styles.info}>
-                      <div>
-                        <FiCalendar />
-                        <time>{ post.first_publication_date }</time>
-                      </div>
-                      <div>
-                        <FiUser />
-                        { post.data.author }
-                      </div>
+            posts.map(post => (
+              <Link href={`/post/${post.uid}`} key={post.uid}>
+                <article>
+                  <strong>{post.data.title}</strong>
+                  <p>{post.data.subtitle}</p>
+
+                  <div className={styles.info}>
+                    <div>
+                      <FiCalendar />
+                      <time>{format(new Date(post.first_publication_date), 'dd MMM yyyy', { locale: ptBR })}</time>
                     </div>
-                  </article>
-                </Link>
-              ))
+                    <div>
+                      <FiUser />
+                      {post.data.author}
+                    </div>
+                  </div>
+                </article>
+              </Link>
+            ))
           }
 
           {
@@ -118,33 +118,31 @@ export default function Home({ postsPagination }: HomeProps) {
 export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient();
 
-  const postsResponse = await prismic.query<any>([
-    Prismic.predicates.at('document.type', 'post')
-  ], {
-    fetch: ['title', 'content'],
-    pageSize: 1,
-  });
+  const postsResponse: PostPagination = await prismic.query<any>(
+    [Prismic.predicates.at('document.type', 'post')],
+    { pageSize: 1 }
+  );
 
-  const posts = postsResponse.results.map(post => {
-    return {
-      uid: post.uid,
-      first_publication_date: new Date(post.first_publication_date).toLocaleDateString('pt-BR', {
-        day: '2-digit', month: 'long', year: 'numeric'
-      }),
-      data: {
-        title: post.data.title,
-        subtitle: post.data.subtitle,
-        author: post.data.author
-      }
-    }
-  })
-
-  const postsPagination = {
-    next_page: postsResponse.next_page,
-    results: posts
-  }
+  // const posts: Post[] = postsResponse.results.map(
+  //   (post): Post => ({
+  //     uid: post.uid,
+  //     first_publication_date: new Date(post.first_publication_date).toLocaleDateString('pt-BR', {
+  //       day: '2-digit', month: 'long', year: 'numeric'
+  //     }),
+  //     data: {
+  //       title: post.data.title,
+  //       subtitle: post.data.subtitle,
+  //       author: post.data.author
+  //     }
+  //   }))
 
   return {
-    props: { postsPagination }
+    props: {
+      postsPagination: {
+        next_page: postsResponse.next_page,
+        results: postsResponse.results
+      }
+    },
+    revalidate: 60 * 60
   }
 };
