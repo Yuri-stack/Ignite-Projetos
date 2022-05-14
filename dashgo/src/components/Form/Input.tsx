7093065861448867cc335756329cb4c3f9bf009e
Yuri-Stack <yurioliveirasilva1999@gmail.com>
2022-05-14 17:42:19 -0300

    Feat.: Adicionando a Page SignIn e Componentizando o Input

---
+++ dashgo/src/components/Form/Input.tsx
@@ -0,0 +1,23 @@
+import { FormControl, FormLabel, Input as ChakraInput, InputProps as ChakraInputProps } from '@chakra-ui/react'
+
+interface InputProps extends ChakraInputProps{
+    name: string
+    label?: string
+}
+
+export function Input({ name, label, ...rest }: InputProps){
+    return (
+        <FormControl>
+            { !!label && <FormLabel htmlFor={ name }>{ label }</FormLabel> }
+            
+            <ChakraInput
+              id={ name } name={ name } type="email"
+              focusBorderColor='pink.500'
+              backgroundColor="gray.900"
+              variant='filled' size="lg"
+              _hover={{ bgColor: "gray.900" }}
+              { ...rest }   // Repassando todas outras props dos Inputs
+            />
+          </FormControl>
+    )
+} 
