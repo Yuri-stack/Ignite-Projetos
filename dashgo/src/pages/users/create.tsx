7ae369bb5ef8370071c454665afe73d4040d8b9a
Yuri-Stack <yurioliveirasilva1999@gmail.com>
2022-05-29 21:59:24 -0300

    Feat.: Cadastrando usuários com Mutations

--- dashgo/src/pages/users/create.tsx
+++ dashgo/src/pages/users/create.tsx
@@ -1,11 +1,19 @@
 import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack } from "@chakra-ui/react";
 import Link from "next/link";
+import { useRouter } from "next/router";
+
+import { useMutation } from 'react-query';
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+import { yupResolver } from '@hookform/resolvers/yup/dist/yup';
+import * as yup from 'yup';
+
 import { Input } from "../../components/Form/Input";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
-import { SubmitHandler, useForm } from 'react-hook-form'
-import { yupResolver } from '@hookform/resolvers/yup/dist/yup';
-import * as yup from 'yup';
+
+import { api } from "../../services/api";
+import { queryClient } from "../../services/queryClient";
 
 type CreateUserFormData = {
     email: string
@@ -24,6 +32,24 @@ const createUserFormSchema = yup.object().shape({
 })
 
 export default function CreateUser() {
+
+    const router = useRouter()
+
+    const createUser = useMutation(async (user: CreateUserFormData) => {
+        const response = await api.post('users', {
+            user: {
+                ...user,
+                created_at: new Date()
+            }
+        })
+
+        return response.data.user
+    }, {
+        onSuccess: () => {
+            queryClient.invalidateQueries('users')
+        }
+    })
+
     const { register, handleSubmit, formState } = useForm({
         resolver: yupResolver(createUserFormSchema)
     })
@@ -31,8 +57,9 @@ export default function CreateUser() {
     const { errors } = formState
 
     const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
-        await new Promise(resolve => setTimeout(resolve, 2000))
-        console.log(values)
+        await createUser.mutateAsync(values)
+
+        router.push('/users')
     }
 
     return (
