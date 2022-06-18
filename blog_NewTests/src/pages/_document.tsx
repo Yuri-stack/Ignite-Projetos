ee28c0e0229ec853aeb18ce7d3cee779850df8e8
Yuri-Stack <yurioliveirasilva1999@gmail.com>
2022-06-18 16:56:25 -0300

    Docs: Add novo Repo

---
+++ blog_NewTests/src/pages/_document.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous' />
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&family=Syne+Tactile&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
