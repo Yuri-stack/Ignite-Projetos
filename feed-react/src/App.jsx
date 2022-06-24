bc8d5d7e1e6c57bf06c48340e6f14f911c17f282
Yuri-Stack <yurioliveirasilva1999@gmail.com>
2022-06-24 19:06:16 -0300

    Feat.: Add Componente com Props

--- feed-react/src/App.jsx
+++ feed-react/src/App.jsx
@@ -3,7 +3,15 @@ import { Post } from "./Post";
 export function App() {
   return (
     <>
-      <Post />
+      <Post
+        author="Yuri"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, consequatur? Facere blanditiis aperiam enim alias eligendi, pariatur recusandae accusantium soluta asperiores facilis, dolores temporibus optio doloremque, ratione sit iusto dolor!"
+      />
+
+      <Post
+        author="Yssac"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, consequatur? Facere blanditiis aperiam enim alias eligendi, pariatur recusandae accusantium soluta asperiores facilis, dolores temporibus optio doloremque, ratione sit iusto dolor!2"
+      />
     </>
   )
 }
