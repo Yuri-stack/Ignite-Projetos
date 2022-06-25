fc5d135c551a7c14374308255ad65f207dbd4edd
Yuri-stack <yurioliveirasilva1999@gmail.com>
2022-06-25 13:32:03 -0300

    Feat.: Add Componente Sidebar

--- feed-react/src/App.jsx
+++ feed-react/src/App.jsx
@@ -1,6 +1,8 @@
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
 import { Post } from "./Post";
 
+import styles from './App.module.css'
 import './global.css'
 
 export function App() {
@@ -8,15 +10,23 @@ export function App() {
     <>
       <Header />
 
-      <Post
-        author="Yuri"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, consequatur? Facere blanditiis aperiam enim alias eligendi, pariatur recusandae accusantium soluta asperiores facilis, dolores temporibus optio doloremque, ratione sit iusto dolor!"
-      />
+      <div className={styles.wrapper}>
+        <aside>
+          <Sidebar />
+        </aside>
+        
+        <main>
+          <Post
+            author="Yuri"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, consequatur? Facere blanditiis aperiam enim alias eligendi, pariatur recusandae accusantium soluta asperiores facilis, dolores temporibus optio doloremque, ratione sit iusto dolor!"
+          />
 
-      <Post
-        author="Yssac"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, consequatur? Facere blanditiis aperiam enim alias eligendi, pariatur recusandae accusantium soluta asperiores facilis, dolores temporibus optio doloremque, ratione sit iusto dolor!2"
-      />
+          <Post
+            author="Yssac"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, consequatur? Facere blanditiis aperiam enim alias eligendi, pariatur recusandae accusantium soluta asperiores facilis, dolores temporibus optio doloremque, ratione sit iusto dolor!2"
+          />
+        </main>
+      </div>
     </>
   )
 }
