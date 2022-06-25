607faa01d0c4940444db5500afbac6461e9043f3
Yuri-stack <yurioliveirasilva1999@gmail.com>
2022-06-25 13:46:35 -0300

    Feat.: Finalizando o Componente Sidebar

--- feed-react/src/components/Sidebar.jsx
+++ feed-react/src/components/Sidebar.jsx
@@ -1,20 +1,27 @@
+import { PencilLine } from 'phosphor-react'
+
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
-  return (
-    <aside className={styles.sidebar}>
-        <img className={styles.cover}
-            src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" 
-        />
+    return (
+        <aside className={styles.sidebar}>
+            <img className={styles.cover}
+                src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+            />
 
-        <div className={styles.profile}>
-            <strong>Yuri Oliveira</strong>
-            <span>Web Developer</span>
-        </div>
+            <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/Yuri-stack.png" />
 
-        <footer>
-            <a href="#">Editar seu Perfil</a>
-        </footer>
-    </aside>
-  )
+                <strong>Yuri Oliveira</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    <PencilLine size={20} />
+                    Editar seu Perfil
+                </a>
+            </footer>
+        </aside>
+    )
 }
