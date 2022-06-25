b4249f20d5aba90d205f68c049c6a8c1abad52a6
Yuri-stack <yurioliveirasilva1999@gmail.com>
2022-06-25 12:55:53 -0300

    Styles: Add Estilos Globais

--- feed-react/src/App.jsx
+++ feed-react/src/App.jsx
@@ -1,8 +1,13 @@
+import { Header } from "./components/Header";
 import { Post } from "./Post";
+
+import './global.css'
 
 export function App() {
   return (
     <>
+      <Header />
+
       <Post
         author="Yuri"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, consequatur? Facere blanditiis aperiam enim alias eligendi, pariatur recusandae accusantium soluta asperiores facilis, dolores temporibus optio doloremque, ratione sit iusto dolor!"
