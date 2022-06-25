fea634dccc2888dfe967c485097c82844b41fdd5
Yuri-stack <yurioliveirasilva1999@gmail.com>
2022-06-25 13:58:09 -0300

    Feat.: Add Componente Post

---
+++ feed-react/src/components/Post.jsx
@@ -0,0 +1,33 @@
+import styles from './Post.module.css'
+
+export function Post() {
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https://github.com/Yuri-stack.png" />
+                    <div className={styles.authorInfo}>
+                        <strong>Yuri Oliveira</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+
+                <time
+                    title='25 de Junho de 2022'
+                    dateTime='2022-06-25 13:53:00'>
+                    Publicado há 1 h
+                </time>
+
+                <div className={styles.content}>
+                    <p>Fala galeraa 👋</p>
+
+                    <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+
+                    <p><a href="">👉 jane.design/doctorcare</a></p>
+
+                    <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+                </div>
+            </header>
+        </article>
+    )
+}
