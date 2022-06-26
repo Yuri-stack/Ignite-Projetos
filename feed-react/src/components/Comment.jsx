eb6f8ede6f6e712cdc84a8abbf921d27c6fb345d
Yuri-stack <yurioliveirasilva1999@gmail.com>
2022-06-26 14:58:33 -0300

    Feat.: Add Componente Comment

---
+++ feed-react/src/components/Comment.jsx
@@ -0,0 +1,41 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img className={styles.avatar}
+                src="https://github.com/Yuri-stack.png"
+                alt=''
+            />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Yuri Oliveira</strong>
+                            <time
+                                title='25 de Junho de 2022'
+                                dateTime='2022-06-25 13:53:00'>
+                                Cerca de 1h atrás
+                            </time>
+                        </div>
+
+                        <button title='Deletar comentário'>
+                            <Trash size={20} />
+                        </button>
+                    </header>
+
+                    <p>Muito bom Devon, parabéns!! 👏👏</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
