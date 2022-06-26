aac8ede58cf2b505017a42f10feef18cb0fc6e83
Yuri-Stack <yurioliveirasilva1999@gmail.com>
2022-06-26 15:37:46 -0300

    Refactor: Add Componente Avatar

--- feed-react/src/components/Sidebar.jsx
+++ feed-react/src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar'
 
 import styles from './Sidebar.module.css'
 
@@ -10,7 +11,7 @@ export function Sidebar() {
             />
 
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/Yuri-stack.png" />
+                <Avatar src="https://github.com/Yuri-stack.png" />
 
                 <strong>Yuri Oliveira</strong>
                 <span>Web Developer</span>
