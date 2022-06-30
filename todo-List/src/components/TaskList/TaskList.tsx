925d00dd79f0c0702b36f8b47339cb9993a09d49
Yuri-Stack <yurioliveirasilva1999@gmail.com>
2022-06-30 18:29:47 -0300

    Feat.: Add TaskList Componente

--- todo-List/src/components/TaskList/TaskList.tsx
+++ todo-List/src/components/TaskList/TaskList.tsx
@@ -1,5 +1,33 @@
+import { ClipboardText, Trash } from 'phosphor-react'
+
+import styles from './TaskList.module.css'
+
 export function TaskList() {
   return (
-    <div>TaskList</div>
+    <section className={styles.tasklistContainer}>
+      <header className={styles.tasklistHeader}>
+        <p>Tarefas Criadas <span>0</span></p>
+        <p>Concluídas <span>0 de 5</span></p>
+      </header>
+
+      {/* <div className={styles.tasklist}>
+        <ClipboardText size={56} weight="thin"/>
+        <p>Você ainda não tem Tarefas Cadastradas</p>
+        <p>Crie tarefas e organize seus items a fazer</p>
+      </div> */}
+
+      <div className={styles.task}>
+        <input type="checkbox" />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi culpa possimus quae necessitatibus amet facilis ut eligendi voluptatibus? Eos quo qui praesentium a optio placeat consequuntur sed. Distinctio, modi nobis!</p>
+        <Trash size={60} weight="thin" />
+      </div>
+
+      <div className={styles.task}>
+        <input type="checkbox" />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi culpa possimus quae necessitatibus amet facilis ut eligendi voluptatibus? Eos quo qui praesentium a optio placeat consequuntur sed. Distinctio, modi nobis!</p>
+        <Trash size={60} weight="thin" />
+      </div>
+
+    </section>
   )
 }
