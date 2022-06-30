f209f4ce5cb44e39b7f5a5110d1ef690f90c94e7
Yuri-stack <yurioliveirasilva1999@gmail.com>
2022-06-30 17:01:36 -0300

    Feat.: Add Componente Input

--- todo-List/src/App.tsx
+++ todo-List/src/App.tsx
@@ -1,11 +1,16 @@
 import { Header } from './components/Header/Header'
+import { Input } from './components/Input/Input'
 
+import styles from './App.module.css'
 import './global.css'
 
 function App() {
   return (
     <>
       <Header />
+      <div className={styles.wrapper}>
+        <Input />
+      </div>
     </>
   )
 }
--- todo-List/src/components/Input/Input.tsx
+++ todo-List/src/components/Input/Input.tsx
@@ -1,5 +1,26 @@
+import { PlusCircle } from 'phosphor-react'
+
+import styles from './Input.module.css'
+
 export function Input() {
+
+  function handleAddNewTask(){
+    console.log('hello')
+  }
+
   return (
-    <div>Input</div>
+    <form className={styles.form} onSubmit={handleAddNewTask}>
+      <input 
+        name='newTask'
+        type="text" 
+        placeholder='Adicione uma nova tarefa'
+        required
+      />
+
+      <button type='submit'>
+        Criar
+        <PlusCircle size={22} />
+      </button>
+    </form>
   )
 }
