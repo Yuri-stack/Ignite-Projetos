d4811e734642191d116b09d13529659c81436873
Yuri-stack <yurioliveirasilva1999@gmail.com>
2022-06-30 21:54:30 -0300

    Styles: Add ScrollBar Personalizada

--- todo-List/src/components/TaskList/TaskList.tsx
+++ todo-List/src/components/TaskList/TaskList.tsx
@@ -5,24 +5,37 @@ import styles from './TaskList.module.css'
 export function TaskList() {
   const checked = true
 
+  let tasks = [1, 2, 3, 4]
+  let quantity = 4
+
   return (
     <section className={styles.tasklistContainer}>
       <header className={styles.tasklistHeader}>
-        <p>Tarefas Criadas <span>0</span></p>
-        <p>Concluídas <span>0 de 5</span></p>
+        <p>Tarefas Criadas <span>{quantity}</span></p>
+        <p>Concluídas <span>0 de {quantity}</span></p>
       </header>
 
-      <div className={styles.tasklist}>
-        <ClipboardText size={56} weight="thin" />
-        <p>Você ainda não tem Tarefas Cadastradas</p>
-        <p>Crie tarefas e organize seus items a fazer</p>
-      </div>
+      {
+        quantity === 0 ? (
 
-      <div className={styles.task}>
-        <input type="checkbox" checked={checked} />
-        <p className={checked ? styles.taskChecked : ''}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi culpa possimus quae necessitatibus amet facilis ut eligendi voluptatibus? Eos quo qui praesentium a optio placeat consequuntur sed. Distinctio, modi nobis!</p>
-        <Trash size={60} weight="thin" />
-      </div>
+          <div className={styles.tasklist}>
+            <ClipboardText size={56} weight="thin" />
+            <p>Você ainda não tem Tarefas Cadastradas</p>
+            <p>Crie tarefas e organize seus items a fazer</p>
+          </div>
+
+        ) : (
+
+          tasks.map(item => (
+            <div key={item} className={styles.task}>
+              <input type="checkbox" checked={checked} />
+              <p className={checked ? styles.taskChecked : ''}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi culpa possimus quae necessitatibus amet facilis ut eligendi voluptatibus? Eos quo qui praesentium a optio placeat consequuntur sed. Distinctio, modi nobis!</p>
+              <Trash size={60} weight="thin" />
+            </div>
+          ))
+
+        )
+      }
 
     </section>
   )
