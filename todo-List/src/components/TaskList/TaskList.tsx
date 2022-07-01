c420f7e675b10700a95c9904a5aa1f0299267f7b
Yuri-stack <yurioliveirasilva1999@gmail.com>
2022-06-30 21:38:38 -0300

    Feat.: Melhorando a Estrutura de Componentes e Estilos

--- todo-List/src/components/TaskList/TaskList.tsx
+++ todo-List/src/components/TaskList/TaskList.tsx
@@ -3,6 +3,8 @@ import { ClipboardText, Trash } from 'phosphor-react'
 import styles from './TaskList.module.css'
 
 export function TaskList() {
+  const checked = true
+
   return (
     <section className={styles.tasklistContainer}>
       <header className={styles.tasklistHeader}>
@@ -10,21 +12,15 @@ export function TaskList() {
         <p>Concluídas <span>0 de 5</span></p>
       </header>
 
-      {/* <div className={styles.tasklist}>
-        <ClipboardText size={56} weight="thin"/>
+      <div className={styles.tasklist}>
+        <ClipboardText size={56} weight="thin" />
         <p>Você ainda não tem Tarefas Cadastradas</p>
         <p>Crie tarefas e organize seus items a fazer</p>
-      </div> */}
-
-      <div className={styles.task}>
-        <input type="checkbox" />
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi culpa possimus quae necessitatibus amet facilis ut eligendi voluptatibus? Eos quo qui praesentium a optio placeat consequuntur sed. Distinctio, modi nobis!</p>
-        <Trash size={60} weight="thin" />
       </div>
 
       <div className={styles.task}>
-        <input type="checkbox" />
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi culpa possimus quae necessitatibus amet facilis ut eligendi voluptatibus? Eos quo qui praesentium a optio placeat consequuntur sed. Distinctio, modi nobis!</p>
+        <input type="checkbox" checked={checked} />
+        <p className={checked ? styles.taskChecked : ''}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi culpa possimus quae necessitatibus amet facilis ut eligendi voluptatibus? Eos quo qui praesentium a optio placeat consequuntur sed. Distinctio, modi nobis!</p>
         <Trash size={60} weight="thin" />
       </div>
 
