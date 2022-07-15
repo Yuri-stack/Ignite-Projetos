de2a810b524efa586ac392fc78f62c34d977813f
Yuri-stack <yurioliveirasilva1999@gmail.com>
2022-07-15 00:00:03 -0300

    Feat.: Aprimorando os inputs

--- timer-react/src/pages/Home/index.tsx
+++ timer-react/src/pages/Home/index.tsx
@@ -19,14 +19,24 @@ export function Home() {
           <TaskInput
             id="task"
             type="text"
+            list="task-suggestions"
             placeholder="Dê um nome para o se projeto"
           />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 3" />
+            <option value="Projeto 2" />
+          </datalist>
 
           <label htmlFor="minutesAmout">durante</label>
           <MinutesAmountInput
             id="minutesAmout"
             type="number"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutos.</span>
