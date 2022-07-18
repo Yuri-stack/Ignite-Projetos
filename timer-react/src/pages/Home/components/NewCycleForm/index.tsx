2f7d250cd5e1510c0e462d9556f371197a5dbd9c
Yuri-Stack <yurioliveirasilva1999@gmail.com>
2022-07-18 20:23:36 -0300

    Feat: Criando Reducer de Ciclos - AddNewCycle

--- timer-react/src/pages/Home/components/NewCycleForm/index.tsx
+++ timer-react/src/pages/Home/components/NewCycleForm/index.tsx
@@ -23,8 +23,8 @@ export function NewCycleForm() {
 
       <datalist id="task-suggestions">
         <option value="Projeto 1" />
-        <option value="Projeto 3" />
         <option value="Projeto 2" />
+        <option value="Projeto 3" />
       </datalist>
 
       <label htmlFor="minutesAmount">durante</label>
@@ -33,7 +33,7 @@ export function NewCycleForm() {
         type="number"
         placeholder="00"
         step={5}
-        min={1}
+        min={5}
         max={60}
         disabled={!!activeCycle}
         {...register('minutesAmount', { valueAsNumber: true })}
