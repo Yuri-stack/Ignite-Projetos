d9a5a21cea2285dad66213d87e6f2c8b0015b71e
Yuri-stack <yurioliveirasilva1999@gmail.com>
2022-09-07 20:54:23 -0300

    Feat.: Aplicando React Hook Form

--- dt-money/src/components/SearchForm/index.tsx
+++ dt-money/src/components/SearchForm/index.tsx
@@ -1,11 +1,34 @@
+import * as z from 'zod'
+import { useForm } from "react-hook-form";
 import { MagnifyingGlass } from "phosphor-react";
+import { zodResolver } from '@hookform/resolvers/zod'
+
 import { SearchFormContainer } from "./styles";
+ 
+// Descrevendo a estrutura do campo a ser pesquisado
+const searchFormSchema = z.object({ query: z.string() })
+
+// Tipando o Form
+type searchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
+    const { register, handleSubmit, formState: { isSubmitting } } = useForm<searchFormInputs>({
+        resolver: zodResolver(searchFormSchema)
+    })
+
+    async function handleSearchTransaction(data: searchFormInputs){
+        await new Promise(resolve => setTimeout(resolve, 2000))
+        console.log(data)
+    }
+
     return (
-        <SearchFormContainer>
-            <input type="text" placeholder="Busque por transações" />
-            <button type="submit">
+        <SearchFormContainer onSubmit={handleSubmit(handleSearchTransaction)}>
+            <input
+                type="text"
+                placeholder="Busque por transações"
+                {...register('query')} />
+
+            <button type="submit" disabled={isSubmitting}>
                 <MagnifyingGlass size={20} />
                 Buscar
             </button>
--- dt-money/src/components/TransactionModal/index.tsx
+++ dt-money/src/components/TransactionModal/index.tsx
@@ -1,8 +1,32 @@
+import * as z from 'zod'
 import * as Dialog from '@radix-ui/react-dialog';
+import { useForm } from "react-hook-form";
 import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react';
+import { zodResolver } from '@hookform/resolvers/zod'
+
 import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles';
 
+// Descrevendo a estrutura do campo a ser pesquisado
+const TransactionFormSchema = z.object({
+    description: z.string(),
+    price: z.number(),
+    category: z.string(),
+    // type: z.enum(['income', 'outcome'])
+})
+
+// Tipando o Form
+type TransactionFormInputs = z.infer<typeof TransactionFormSchema>
+
 export function TransactionModal() {
+    const { register, handleSubmit, formState: { isSubmitting } } = useForm<TransactionFormInputs>({
+        resolver: zodResolver(TransactionFormSchema)
+    })
+
+    async function handleCreateTransaction(data: TransactionFormInputs){
+        await new Promise(resolve => setTimeout(resolve, 2000))
+        console.log(data)
+    }
+
     return (
         <Dialog.Portal>
             <Overlay />
@@ -14,24 +38,24 @@ export function TransactionModal() {
                     <X size={24} />
                 </CloseButton>
 
-                <form action="">
-                    <input type="text" placeholder='Descrição' required />
-                    <input type="number" placeholder='Preço' required />
-                    <input type="text" placeholder='Categoria' required />
+                <form onSubmit={ handleSubmit(handleCreateTransaction) }>
+                    <input { ...register('description') } type="text" placeholder='Descrição' required />
+                    <input { ...register('price', { valueAsNumber: true }) } type="number" placeholder='Preço' required />
+                    <input { ...register('category') } type="text" placeholder='Categoria' required />
 
                     <TransactionType>
                         <TransactionTypeButton variant='income' value='income'>
-                            <ArrowCircleUp size={24}/>
+                            <ArrowCircleUp size={24} />
                             Entrada
                         </TransactionTypeButton>
 
                         <TransactionTypeButton variant='outcome' value='outcome'>
-                            <ArrowCircleDown size={24}/>
+                            <ArrowCircleDown size={24} />
                             Saída
                         </TransactionTypeButton>
                     </TransactionType>
 
-                    <button type="submit">Cadastrar</button>
+                    <button type="submit" disabled={ isSubmitting }>Cadastrar</button>
                 </form>
 
             </Content>
