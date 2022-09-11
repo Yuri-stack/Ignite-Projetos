2e1f4377fb94b429fc8860586a19e4a01f64f9fa
Yuri-stack <yurioliveirasilva1999@gmail.com>
2022-09-11 17:33:49 -0300

    Feat.: Add lógica para Controlled Components

--- dt-money/src/components/TransactionModal/index.tsx
+++ dt-money/src/components/TransactionModal/index.tsx
@@ -1,6 +1,6 @@
 import * as z from 'zod'
 import * as Dialog from '@radix-ui/react-dialog';
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react';
 import { zodResolver } from '@hookform/resolvers/zod'
 
@@ -11,18 +11,19 @@ const TransactionFormSchema = z.object({
     description: z.string(),
     price: z.number(),
     category: z.string(),
-    // type: z.enum(['income', 'outcome'])
+    type: z.enum(['income', 'outcome'])
 })
 
 // Tipando o Form
 type TransactionFormInputs = z.infer<typeof TransactionFormSchema>
 
 export function TransactionModal() {
-    const { register, handleSubmit, formState: { isSubmitting } } = useForm<TransactionFormInputs>({
-        resolver: zodResolver(TransactionFormSchema)
+    const { control, register, handleSubmit, formState: { isSubmitting } } = useForm<TransactionFormInputs>({
+        resolver: zodResolver(TransactionFormSchema),
+        defaultValues: { type: 'income' }
     })
 
-    async function handleCreateTransaction(data: TransactionFormInputs){
+    async function handleCreateTransaction(data: TransactionFormInputs) {
         await new Promise(resolve => setTimeout(resolve, 2000))
         console.log(data)
     }
@@ -38,24 +39,33 @@ export function TransactionModal() {
                     <X size={24} />
                 </CloseButton>
 
-                <form onSubmit={ handleSubmit(handleCreateTransaction) }>
-                    <input { ...register('description') } type="text" placeholder='Descrição' required />
-                    <input { ...register('price', { valueAsNumber: true }) } type="number" placeholder='Preço' required />
-                    <input { ...register('category') } type="text" placeholder='Categoria' required />
+                <form onSubmit={handleSubmit(handleCreateTransaction)}>
+                    <input {...register('description')} type="text" placeholder='Descrição' required />
+                    <input {...register('price', { valueAsNumber: true })} type="number" placeholder='Preço' required />
+                    <input {...register('category')} type="text" placeholder='Categoria' required />
 
-                    <TransactionType>
-                        <TransactionTypeButton variant='income' value='income'>
-                            <ArrowCircleUp size={24} />
-                            Entrada
-                        </TransactionTypeButton>
+                    {/* Trabalhando com campos não nativos do Html - usando Controlled Components */}
+                    <Controller
+                        control={control}
+                        name="type"
+                        render={({ field }) => {
+                            return (
+                                <TransactionType onValueChange={ field.onChange } value={ field.value }>
+                                    <TransactionTypeButton variant='income' value='income'>
+                                        <ArrowCircleUp size={24} />
+                                        Entrada
+                                    </TransactionTypeButton>
 
-                        <TransactionTypeButton variant='outcome' value='outcome'>
-                            <ArrowCircleDown size={24} />
-                            Saída
-                        </TransactionTypeButton>
-                    </TransactionType>
+                                    <TransactionTypeButton variant='outcome' value='outcome'>
+                                        <ArrowCircleDown size={24} />
+                                        Saída
+                                    </TransactionTypeButton>
+                                </TransactionType>
+                            )
+                        }}
+                    />
 
-                    <button type="submit" disabled={ isSubmitting }>Cadastrar</button>
+                    <button type="submit" disabled={isSubmitting}>Cadastrar</button>
                 </form>
 
             </Content>
