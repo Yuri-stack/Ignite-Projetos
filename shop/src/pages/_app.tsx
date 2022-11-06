c915231733d334dd81e0c4a518e6c7acfc294cb8
Yuri-stack <yurioliveirasilva1999@gmail.com>
2022-11-06 13:35:53 -0300

    Feat.: Cabeçalho da aplicação

--- shop/src/pages/_app.tsx
+++ shop/src/pages/_app.tsx
@@ -1,8 +1,19 @@
 import type { AppProps } from 'next/app'
 import { globalStyles } from '../styles/global'
+import Image from 'next/image'
+import { Container, Header } from '../styles/pages/app'
+
+import logoImg from '../assets/logo.svg'
+
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  globalStyles()
-  
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt=""/>
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
