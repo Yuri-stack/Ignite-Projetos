a4b70396c37789f42f9ac31775e5f128475bb515
Yuri-stack <yurioliveirasilva1999@gmail.com>
2022-12-01 18:18:02 -0300

    Feat.: Add Estrutura da página de produto

--- shop/src/pages/product/[id].tsx
+++ shop/src/pages/product/[id].tsx
@@ -1,9 +1,23 @@
 import { useRouter } from "next/router"
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
 
 export default function Product() {
   const { query } = useRouter()
 
   return (
-    <h1>Product</h1>
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta 1</h1>
+        <span>R$ 79,99</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime nobis in asperiores iure nihil eum non culpa consectetur nesciunt porro animi labore accusamus, libero officia voluptas sequi? Molestiae, doloribus dolores!
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
