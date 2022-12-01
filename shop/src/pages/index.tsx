ed4c30c05b27e6b2bfb0fa0131846d4380ca2828
Yuri-stack <yurioliveirasilva1999@gmail.com>
2022-12-01 18:01:30 -0300

    Feat.: Add Navegação via Link

--- shop/src/pages/index.tsx
+++ shop/src/pages/index.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
 import Stripe from "stripe";
+import Link from "next/link";
 import { GetStaticProps } from "next";
 import { useKeenSlider } from 'keen-slider/react';
 import { HomeContainer, Product } from "../styles/pages/home";
 
 import { stripe } from '../lib/stripe'
-import camiseta1 from '../assets/camisetas/1.png'
-
 import 'keen-slider/keen-slider.min.css';
 
 interface HomeProps {
@@ -28,13 +27,15 @@ export default function Home({ products }: HomeProps) {
       {
         products.map(product => {
           return (
-            <Product key={ product.id} className="keen-slider__slide">
-              <Image src={camiseta1} width={520} height={480} alt="" />
-              <footer>
-                <strong>{ product.name }</strong>
-                <span>{ product.price }</span>
-              </footer>
-            </Product>
+            <Link href={`/product/${product.id}`} key={product.id}>
+              <Product className="keen-slider__slide">
+                <Image src={product.imageUrl} width={520} height={480} alt="" />
+                <footer>
+                  <strong>{product.name}</strong>
+                  <span>{product.price}</span>
+                </footer>
+              </Product>
+            </Link>
           )
         })
       }
