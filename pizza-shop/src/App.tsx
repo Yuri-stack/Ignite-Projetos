2cc02a536901208573a187bbd748b856df9a7ba3
Yuri-stack <yurioliveirasilva1999@gmail.com>
2024-01-27 21:33:14 -0300

    Config.: Instalando shadcn/ui

--- pizza-shop/src/App.tsx
+++ pizza-shop/src/App.tsx
@@ -1,5 +1,10 @@
+import { Button } from './components/ui/button'
+import './global.css'
+
 export function App() {
 	return (
-		<></>
+		<div className='flex justify-center items-center h-screen'>
+			<Button>Enviar</Button>
+		</div>
 	)
 }
