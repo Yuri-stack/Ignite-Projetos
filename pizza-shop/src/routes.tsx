9ce4187aad6521fd8ae2e246181d87cc42aeaf7f
Yuri-stack <yurioliveirasilva1999@gmail.com>
2024-01-28 21:47:29 -0300

    Feat.: Página 404 (Not Found)

--- pizza-shop/src/routes.tsx
+++ pizza-shop/src/routes.tsx
@@ -5,11 +5,13 @@ import { Dashboard } from "./pages/app/dashboard/dashboard";
 import { SignIn } from "./pages/auth/sign-in";
 import { SignUp } from "./pages/auth/sign-up";
 import { Orders } from "./pages/app/orders/orders";
+import { NotFound } from "./pages/notFound";
 
 export const router = createBrowserRouter([
     {
         path: '/',
         element: <AppLayout />,
+        errorElement: <NotFound />,
         children: [
             { path: '/', element: <Dashboard /> },
             { path: '/orders', element: <Orders /> },
