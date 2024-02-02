4b5e778277cf7c4a03d3973849a9f4b0f376c7f9
Yuri-stack <yurioliveirasilva1999@gmail.com>
2024-02-01 22:57:14 -0300

    Feat.: Configurando API client

--- pizza-shop/src/App.tsx
+++ pizza-shop/src/App.tsx
@@ -1,9 +1,12 @@
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
 
-import { ThemeProvider } from './components/theme/theme-provider'
-import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { Toaster } from 'sonner'
+import { queryClient } from './lib/react-query'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { ThemeProvider } from './components/theme/theme-provider'
+
 import './global.css'
 
 export function App() {
@@ -12,7 +15,9 @@ export function App() {
 			<ThemeProvider defaultTheme="system" storageKey="pizzashop-theme">
 				<Helmet titleTemplate='%s | Pizza Shop' />
 				<Toaster richColors />
-				<RouterProvider router={router} />
+				<QueryClientProvider client={queryClient}>
+					<RouterProvider router={router} />
+				</QueryClientProvider>
 			</ThemeProvider>
 		</HelmetProvider>
 	)
