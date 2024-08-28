e36a18ac3735f5b3ced349bde69c456274f3f137
Yuri-stack <yurioliveirasilva1999@gmail.com>
2024-08-28 11:45:56 -0300

    Feat.: Modal de atualização do perfil

--- pizza-shop/src/components/account-menu.tsx
+++ pizza-shop/src/components/account-menu.tsx
@@ -5,6 +5,8 @@ import { useQuery } from "@tanstack/react-query";
 import { getProfile } from "@/api/get-profile";
 import { getManagedRestaurant } from "@/api/get-managed-restaurant";
 import { Skeleton } from "./ui/skeleton";
+import { Dialog, DialogTrigger } from "./ui/dialog";
+import { StoreProfileDialog } from "./store-profile-dialog";
 
 export function AccountMenu() {
     const { data: profile, isLoading: isLoadingProfile } = useQuery({
@@ -18,45 +20,53 @@ export function AccountMenu() {
     })
 
     return (
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <Button variant="outline" className="flex items-center gap-2 select-none">
+        <Dialog>
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant="outline" className="flex items-center gap-2 select-none">
 
-                    {isLoadingManagedRestaurant ?
-                        <Skeleton className="h-4 w-40" /> : managedRestaurant?.name}
+                        {isLoadingManagedRestaurant ?
+                            <Skeleton className="h-4 w-40" /> : managedRestaurant?.name}
 
-                    <ChevronDown />
-                </Button>
-            </DropdownMenuTrigger>
+                        <ChevronDown />
+                    </Button>
+                </DropdownMenuTrigger>
 
-            <DropdownMenuContent align="end" className="w-56">
-                <DropdownMenuLabel className="flex flex-col">
-                    {isLoadingProfile ? (
-                        <div className="space-y-1.5">
-                            <Skeleton className="h-4 w-32" />
-                            <Skeleton className="h-3 w-32" />
-                        </div>
-                    ) : (
-                        <>
-                            <span>{profile?.name}</span>
-                            <span className="text-xs font-normal text-muted-foreground">
-                                {profile?.email}
-                            </span>
-                        </>
-                    )}
-                </DropdownMenuLabel>
+                <DropdownMenuContent align="end" className="w-56">
+                    <DropdownMenuLabel className="flex flex-col">
+                        {isLoadingProfile ? (
+                            <div className="space-y-1.5">
+                                <Skeleton className="h-4 w-32" />
+                                <Skeleton className="h-3 w-32" />
+                            </div>
+                        ) : (
+                            <>
+                                <span>{profile?.name}</span>
+                                <span className="text-xs font-normal text-muted-foreground">
+                                    {profile?.email}
+                                </span>
+                            </>
+                        )}
+                    </DropdownMenuLabel>
 
-                <DropdownMenuSeparator />
+                    <DropdownMenuSeparator />
 
-                <DropdownMenuItem>
-                    <Building className="mr-2 h-4 w-4" />
-                    <span>Perfil da Loja</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
-                    <LogOut className="mr-2 h-4 w-4" />
-                    <span>Sair</span>
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
+                    <DialogTrigger asChild>
+                        <DropdownMenuItem>
+                            <Building className="mr-2 h-4 w-4" />
+                            <span>Perfil da Loja</span>
+                        </DropdownMenuItem>
+                    </DialogTrigger>
+
+                    <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
+                        <LogOut className="mr-2 h-4 w-4" />
+                        <span>Sair</span>
+                    </DropdownMenuItem>
+
+                </DropdownMenuContent>
+            </DropdownMenu>
+
+            <StoreProfileDialog />
+        </Dialog>
     )
 }
