d297ec75be2d8fa8a52f69216fd7b795bdc5211e
Yuri-stack <yurioliveirasilva1999@gmail.com>
2024-08-27 22:56:48 -0300

    Feat.: Perfil no cabeçalho

--- pizza-shop/src/components/account-menu.tsx
+++ pizza-shop/src/components/account-menu.tsx
@@ -1,22 +1,35 @@
 import { Building, ChevronDown, LogOut } from "lucide-react";
 import { Button } from "./ui/button";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import { useQuery } from "@tanstack/react-query";
+import { getProfile } from "@/api/get-profile";
+import { getManagedRestaurant } from "@/api/get-managed-restaurant";
 
 export function AccountMenu() {
+    const { data: profile } = useQuery({
+        queryKey: ['profile'],
+        queryFn: getProfile
+    })
+
+    const { data: managedRestaurant } = useQuery({
+        queryKey: ['managed-restaurant'],
+        queryFn: getManagedRestaurant
+    })
+
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
                 <Button variant="outline" className="flex items-center gap-2 select-none">
-                    Pizza Shop
+                    {managedRestaurant?.name}
                     <ChevronDown />
                 </Button>
             </DropdownMenuTrigger>
 
             <DropdownMenuContent align="end" className="w-56">
                 <DropdownMenuLabel className="flex flex-col">
-                    <span>Yuri Oliveira</span>
+                    <span>{profile?.name}</span>
                     <span className="text-xs font-normal text-muted-foreground">
-                        yuri@oliveira.com
+                        {profile?.email}
                     </span>
                 </DropdownMenuLabel>
 
