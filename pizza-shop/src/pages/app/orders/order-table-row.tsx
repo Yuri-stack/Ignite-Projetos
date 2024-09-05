a7ad7cf9ac371d562a798deea18f62f7ed5ba251
Yuri-stack <yurioliveirasilva1999@gmail.com>
2024-09-04 21:59:55 -0300

    Feat.: Cancelar pedido

--- pizza-shop/src/pages/app/orders/order-table-row.tsx
+++ pizza-shop/src/pages/app/orders/order-table-row.tsx
@@ -7,6 +7,9 @@ import { OrderStatus } from "@/components/order-status";
 import { formatDistanceToNow } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 import { useState } from "react";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { cancelOrder } from "@/api/cancel-order";
+import { GetOrdersResponse } from "@/api/get-orders";
 
 export interface OrderTableRowProps {
     order: {
@@ -20,6 +23,29 @@ export interface OrderTableRowProps {
 
 export function OrderTableRow({ order }: OrderTableRowProps) {
     const [isDetailsOpen, setIsDetailsOpen] = useState(false)
+    const queryClient = useQueryClient()
+
+    const { mutateAsync: cancelOrderFn } = useMutation({
+        mutationFn: cancelOrder,
+        onSuccess(_, { orderId }) {
+            const ordersListCache = queryClient.getQueriesData<GetOrdersResponse>({
+                queryKey: ['orders']
+            })
+
+            ordersListCache.forEach(([cacheKey, cacheData]) => {
+                if (!cacheData) return
+
+                queryClient.setQueryData<GetOrdersResponse>(cacheKey, {
+                    ...cacheData,
+                    orders: cacheData.orders.map(order => {
+                        if (order.orderId === orderId) return { ...order, status: 'canceled' }
+
+                        return order
+                    })
+                })
+            })
+        },
+    })
 
     return (
         <TableRow>
@@ -50,7 +76,7 @@ export function OrderTableRow({ order }: OrderTableRowProps) {
                 {order.customerName}
             </TableCell>
             <TableCell className="font-medium">
-                {order.total.toLocaleString('pt-BR', {
+                {(order.total / 100).toLocaleString('pt-BR', {
                     style: 'currency',
                     currency: 'BRL'
                 })}
@@ -62,7 +88,12 @@ export function OrderTableRow({ order }: OrderTableRowProps) {
                 </Button>
             </TableCell>
             <TableCell>
-                <Button variant="ghost" size="xs">
+                <Button
+                    onClick={() => cancelOrderFn({ orderId: order.orderId })}
+                    disabled={!['pending', 'processing'].includes(order.status)}
+                    variant="ghost"
+                    size="xs"
+                >
                     <X className="mr-2 h-3 w-3" />
                     Cancelar
                 </Button>
